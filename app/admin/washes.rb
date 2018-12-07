b5f96a8e5a286f1caa1e6bcf28a5e97dbf2aa398
nicoalamo <nicoalamo@gmail.com>
2018-12-07 17:28:43 -0300

    changed active admin view for washes

--- app/admin/washes.rb
+++ app/admin/washes.rb
@@ -2,8 +2,20 @@ ActiveAdmin.register Wash do
 
   index do
     column :id
+    column :wash_type do |wash|
+      wash.wash_type.name
+    end
     column :patent do |wash|
       wash.vehicle.patent
+    end
+    column :patent do |wash|
+      wash.vehicle.vehicle_type
+    end
+    column :patent do |wash|
+      wash.vehicle.brand
+    end
+    column :patent do |wash|
+      wash.vehicle.model
     end
     column :user do |wash|
       wash.vehicle.user.first_name + " " + wash.vehicle.user.last_name
