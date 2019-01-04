44c26f62bb52a4ec83000dd71d68de6c8969542e
nicoalamo <nicoalamo@gmail.com>
2019-01-03 22:06:19 -0300

    fixed sidebar and started edit page

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :washers, only: [:index, :show]
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show, :edit] do
     resources :vehicles
     resources :washes
     resources :receipts
